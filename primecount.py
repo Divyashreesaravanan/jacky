4f7110dff350c287adfcf872b7509021194d67fc
Divyashreesaravanan <divyashree.it13@bitsathy.ac.in>
2016-07-11 19:10:35 +0530

    Create primecount.py

---
+++ primecount.py
@@ -0,0 +1,10 @@
+count=0
+for n in range(1,10):
+    for i in range(2,n):
+        if(n%2==0):
+            break
+        else:
+            count=count+1
+            print(count)
+            break
+            print(count)
